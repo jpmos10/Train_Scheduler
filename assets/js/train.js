1a6eb9b1f5343a0794ea3386aa2f60144a11e1e4
Jason Mosley <jpmos10@users.noreply.github.com>
2016-09-16 21:58:03 -0500

    added firebase to index.html and train.js and renamed dir javascript to dir js

---
+++ assets/js/train.js
@@ -0,0 +1,13 @@
+
+
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCM2pxkn44c0qOTCKRzgd9fDa_8_d9X83Q",
+    authDomain: "traintime-536df.firebaseapp.com",
+    databaseURL: "https://traintime-536df.firebaseio.com",
+    storageBucket: "traintime-536df.appspot.com",
+    messagingSenderId: "589897693668"
+  };
+  firebase.initializeApp(config);
+</script>
